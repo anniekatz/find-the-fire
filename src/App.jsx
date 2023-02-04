cd48745306d98127591a4ecb96717456f9fd9e65
Annie Katz <akatz.dev@gmail.com>
2023-02-03 21:10:33 -0500

    Updated card app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const tiers = [
+  { size: 2, happyFace: Math.floor(Math.random() * 2) },
+  { size: 3, happyFace: Math.floor(Math.random() * 3) },
+  { size: 4, happyFace: Math.floor(Math.random() * 4) },
+  { size: 4, happyFace: Math.floor(Math.random() * 4) }
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tier, setTier] = useState(0);
+  const [isWin, setIsWin] = useState(false);
+  const [isLose, setIsLose] = useState(false);
+  const [isFlipping, setIsFlipping] = useState(false);
+
+  const handleCardClick = index => {
+    if (index === tiers[tier].happyFace) {
+      setIsFlipping(true);
+      setIsWin(true);
+      setTimeout(() => {
+        setIsFlipping(false);
+        setIsWin(false);
+        setTier(tier + 1);
+      }, 1000);
+    } else {
+      setIsFlipping(true);
+      setIsLose(true);
+    }
+  };
+
+  useEffect(() => {
+    if (tier === tiers.length) {
+      alert("You win the game!");
+    }
+  }, [tier]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {tier < tiers.length ? (
+        <div className="tier">
+          {Array.from({ length: tiers[tier].size }, (_, i) => (
+            <div
+              key={i}
+              className={`card ${isFlipping ? "flipping" : ""} ${
+                i === tiers[tier].happyFace ? "correct" : "incorrect"
+              } ${isWin ? "win" : ""} ${isLose && isFlipping ? "lose" : ""}`}
+              onClick={() => handleCardClick(i)}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>You win the game!</p>
+      )}
+      {isLose && (
+        <p className="game-over">Game Over</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
