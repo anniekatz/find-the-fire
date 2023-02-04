5c1faf625841e7ef1ca35fc34c1df3406d8b6d1a
Annie Katz <akatz.dev@gmail.com>
2023-02-04 12:02:52 -0500

    Updated second app

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,56 +1,76 @@
 import React, { useState } from "react";
-import Card from "./Card";
-import "./Card.css";
+import "./App2.css";
 
-const App = () => {
-  const [level, setLevel] = useState(1);
-  const [happyFaceIndex, setHappyFaceIndex] = useState(0);
-  const [gameOver, setGameOver] = useState(false);
+const Card = ({ value, flipped, handleClick }) => (
+  <div
+    className={`card ${flipped ? "flipped" : ""}`}
+    onClick={() => handleClick(value)}
+  >
+    <div className="front">?</div>
+    <div className="back">{value === "happy" ? "😊" : "😢"}</div>
+  </div>
+);
 
-  const startNewGame = () => {
-    setLevel(1);
-    setHappyFaceIndex(Math.floor(Math.random() * level));
-    setGameOver(false);
+const Game = ({ level, onWin, onLose }) => {
+  const [cards, setCards] = useState(
+    Array.from({ length: level }, () => "hidden")
+  );
+  const [selected, setSelected] = useState(-1);
+
+  const handleClick = (value, index) => {
+    setCards(
+      cards.map((v, i) => (i === index ? value : v))
+    );
+    setSelected(index);
   };
 
-  const handleCardClick = index => {
-    if (index === happyFaceIndex) {
-      if (level === 3) {
-        alert("You win!");
-        startNewGame();
-      } else {
-        setLevel(level + 1);
-        setHappyFaceIndex(Math.floor(Math.random() * level));
-      }
-    } else {
-      setGameOver(true);
-    }
+  const handleReset = () => {
+    setCards(
+      Array.from({ length: level }, () => "hidden")
+    );
+    setSelected(-1);
   };
+
+  if (selected === -1) {
+    const happyIndex = Math.floor(Math.random() * level);
+    return (
+      <div className="game">
+        {Array.from({ length: level }).map((_, i) => (
+          <Card
+            key={i}
+            value={i === happyIndex ? "happy" : "sad"}
+            flipped={false}
+            handleClick={(value) => handleClick(value, i)}
+          />
+        ))}
+      </div>
+    );
+  } else if (cards[selected] === "happy") {
+    setTimeout(() => onWin(), 1000);
+    return null;
+  } else {
+    setTimeout(() => onLose(), 1000);
+    return (
+      <div className="game">
+        <div>Game over</div>
+        <button onClick={handleReset}>Try Again</button>
+      </div>
+    );
+  }
+};
+
+const App2 = () => {
+  const [level, setLevel] = useState(2);
+
+  const handleWin = () => setLevel(level + 1);
+  const handleLose = () => setLevel(1);
 
   return (
-    <div className="game">
-      {gameOver ? (
-        <div>
-          <p>Game over</p>
-          <button onClick={startNewGame}>Start new game</button>
-        </div>
-      ) : (
-        <>
-          <p>Level {level}</p>
-          {Array.from({ length: level }, (_, index) => (
-            <Card
-              key={index}
-              level={level}
-              handleClick={() => handleCardClick(index)}
-              flipped={index === happyFaceIndex}
-              front={"📎"}
-              back={index === happyFaceIndex ? "😊" : "😢"}
-            />
-          ))}
-        </>
-      )}
+    <div className="App">
+      <Game level={level} onWin={handleWin} onLose={handleLose} />
     </div>
   );
 };
 
-export default App;
+export default App2;
+
